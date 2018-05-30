58d03ed36e4a517812860b3205b6ac72d4ff9157
RAJ MOHAMMED <raaz.mohd@gmail.com>
2018-05-30 11:47:37 +0530

    Add files via upload

---
+++ Assignments/Week 2/js/app1.js
@@ -0,0 +1,48 @@
+function identifyById() {
+    //  console.log(document.getElementById('para1'));
+     alert(document.getElementById('para1'));
+    }
+    function identifyByClass() {
+        alert(document.getElementByClassName('class1'));
+     //   console.log(document.getElementsByClassName('class1'));
+        }
+        
+        function identifyByTag() {
+        alert(document.getElementsByTagName('p'));
+        }
+        
+        function changeBackground() {
+        document.getElementById('block1').style.backgroundColor="blue";
+        }
+        
+        function increaseFont() {
+            document.getElementById('block2').style.fontSize="xx-large";
+        }
+        
+        function changeFontColor() {
+        
+        }
+        
+        function revertColor() {
+        
+        }
+        
+        function hide() {
+        
+        }
+        
+        function changeBackColorForAllBox() {
+        
+        }
+        
+        
+        function addAdjacent() {
+        }
+        
+        function removePara() {
+        
+        }
+        
+        function animatePara() {
+        
+        }
